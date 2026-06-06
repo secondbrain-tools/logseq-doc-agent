6a28f0c8049bdcb7655e04ee07b9a752ae16045e
Stefan Kaufmann <stefan.kaufmann@it-designers.de>
2026-06-06 09:34:45 +0200

    fix iconsize

--- src/ui/icons.ts
+++ src/ui/icons.ts
@@ -1,15 +1,15 @@
 export const ICONS = {
-  maximize: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  maximize: `
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <path d="M4 8v-2a2 2 0 0 1 2 -2h2"></path>
         <path d="M4 16v2a2 2 0 0 0 2 2h2"></path>
         <path d="M16 4h2a2 2 0 0 1 2 2v2"></path>
         <path d="M16 20h2a2 2 0 0 0 2 -2v-2"></path>
-    </svg>`,
-  restore: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    `,
+  restore: `
         <rect x="3" y="11" width="10" height="10" rx="2" ry="2"></rect>
         <path d="M7 11V7a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-4"></path>
-    </svg>`,
+    `,
   close: `
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
